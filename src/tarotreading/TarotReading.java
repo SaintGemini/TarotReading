8648772d1bd20e12e85b41ce86cd8fe3ba250757
SaintGemini <chacone96@gmail.com>
2018-10-03 20:18:34 -0700

    Update TarotReading.java

--- src/tarotreading/TarotReading.java
+++ src/tarotreading/TarotReading.java
@@ -1,28 +1,21 @@
-
-package tarotreading;
 import java.util.Random;
 
 public class TarotReading {
 
-    
-
     public static void main(String[] args) {
+        //for the reversed output on lines 24-32
         Random reverse = new Random();
         int rando;
 
         TarotDeck tarot = new TarotDeck();
         TarotDeck reading = new TarotDeck();
         
-        tarot.createMinorArcana();
-        tarot.createMajorArcana();
-
-        
-        for(int i = 0; i < 4; i ++){
+        tarot.createTarotDeck();
         tarot.shuffle();
-        }
         
-        reading.drawFromDeck(tarot);
+        reading.threeCardReading(tarot);
         
+        //creates a 50% chance the card drawn will be reversed or normal
         for(int i = 0; i < reading.getSize(); i++) {
             rando = reverse.nextInt(2);
             if(rando == 1){
